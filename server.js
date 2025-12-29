d3d549a1a86118aadaa19ddf09fb43c42affe558
Sadat-Khadija <khadija.sadat9815@gmail.com>
2025-12-28 23:33:37 -0800

    added the model

---
+++ server.js
@@ -0,0 +1,25 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+const Book = require('./models/book.js');
+
+
+app.get('/', async(req, res) => {
+  res.render('index.ejs');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
